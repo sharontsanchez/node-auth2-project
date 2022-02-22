adb04e6902c17abdb475c9c9640817481c0e2a90
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-22 13:25:25 -0600

    [POST] /api/auth/register has been set up

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const bcrypt = require('bcryptjs')
+const User = require('../users/users-model')
 
-router.post("/register", validateRoleName, (req, res, next) => {
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
@@ -14,6 +15,16 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+
+router.post("/register", validateRoleName, (req, res, next) => {
+  const { username, password } = req.body
+  const { role_name } = req
+  const hash = bcrypt.hashSync(password, 8)
+  User.add({username, password: hash, role_name})
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(next)
 });
 
 
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -81,7 +81,7 @@ function findBy(filter) {
 function findById(user_id) {
   return db('users')
     .join('roles', 'users.role_id', 'roles.role_id')
-    .select('user_id', 'username', 'password', 'role_name')
+    .select('user_id', 'username','role_name')
     .where('users.user_id', user_id).first()
 }
 
